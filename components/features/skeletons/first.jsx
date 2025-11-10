2f4b9f80a421720b12fe85c0663d9f0f4a15282e
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-11-10 14:00:14 +0530

    added

--- components/features/skeletons/first.jsx
+++ components/features/skeletons/first.jsx
@@ -3,21 +3,45 @@ import { IconCircleDashedCheck, IconClock, IconRipple } from '@tabler/icons-reac
 
 const SkeletonOne = () => {
   return (
-    <div><SkeletonCard /></div>
+    <div>
+        <SkeletonCard icon={ <IconCircleDashedCheck className='size-4' />} title='Campaign Planner'
+        description='Creates clear, ready-to-use campaign briefs using product info, audience data, and past results.'
+        badge={<Badge text='120S' variant='danger' />} />
+    </div>
   )
 }
 
 export default SkeletonOne
 
-const SkeletonCard = () => {
+// const SkeletonCard = ({icon, title, description, badge}) => {
+//    return (
+//      <div className='max-w-[90%] mx-auto w-full p-3 rounded-lg bg-neutral-100 border border-neutral-100 dark:border-neutral-700'>
+//         <div className='flex gap-3 items-center'>
+//             <IconCircleDashedCheck className='size-4' />
+//             <p className='text-sm font-bold'>Campaign Planner</p>
+//             <Badge text='120S' variant='danger' />
+//         </div>
+//         <p className='text-sm text-neutral-500 dark:text-neutral-400 font-medium mt-3'>Creates clear, ready-to-use campaign briefs using product info, audience data, and past results.</p>
+//         <div className='flex items-center gap-2 flex-wrap mt-4'>
+//             <Tag text='Google Ads' />
+//             <Tag text='SaaS' />
+//             <Tag text='Content' />
+//         </div>
+//     </div>
+//    )
+// }
+
+const SkeletonCard = ({icon, title, description, badge}) => {
    return (
      <div className='max-w-[90%] mx-auto w-full p-3 rounded-lg bg-neutral-100 border border-neutral-100 dark:border-neutral-700'>
         <div className='flex gap-3 items-center'>
-            <IconCircleDashedCheck className='size-4' />
-            <p className='text-sm font-bold'>Campaign Planner</p>
-            <Badge text='120S' variant='danger' />
+            {icon}
+            <p className='text-sm font-bold'>{title}</p>
+            {badge}
         </div>
-        <p className='text-sm text-neutral-500 dark:text-neutral-400 font-medium mt-3'>Creates clear, ready-to-use campaign briefs using product info, audience data, and past results.</p>
+        <p className='text-sm text-neutral-500 dark:text-neutral-400 font-medium mt-3'>
+            {description}
+        </p>
         <div className='flex items-center gap-2 flex-wrap mt-4'>
             <Tag text='Google Ads' />
             <Tag text='SaaS' />
